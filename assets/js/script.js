ced14a1d33b13f207852b519060b83d9de6ba8ac
Xx-Kentaro-xX <kentravel0328@gmail.com>
2022-06-11 23:55:10 +0900

    deletion and creation file due to wrong file name

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// ページトップ戻るボタン制御
+function pageTopButtonControl() {
+  try {
+    let pageTopButton = document.querySelector(".page-top-button");
+    pageTopButton.addEventListener("click", function () {
+      scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    });
+
+    window.addEventListener("scroll", function () {
+      let currentScrollY = window.scrollY;
+
+      if (currentScrollY > 300) {
+        pageTopButton.style.display = "block";
+      } else {
+        pageTopButton.style.display = "none";
+      }
+    });
+  } catch (e) {}
+}
+
+// 呼び出し
+pageTopButtonControl();
