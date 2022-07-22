d5d7559dbe56b0a9dd8ce53891457844532792a9
Xx-Kentaro-xX <kentravel0328@gmail.com>
2022-07-23 00:13:38 +0900

    modify new article button position issue on mobile

--- sub_parts/mobile-top-nav.php
+++ sub_parts/mobile-top-nav.php
@@ -4,9 +4,11 @@
       <a href="#about-blog">
         <li>ブログについて</li>
       </a>
+      <li class="menu-separetor">|</li>
       <a href="#category">
         <li>カテゴリー</li>
       </a>
+      <li class="menu-separetor">|</li>
       <a href="#about-author">
         <li>筆者について</li>
       </a>
